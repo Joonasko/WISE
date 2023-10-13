19c04214ebec57cf2a0e18a605964de76573b016
Joonas Kolstela <joonas.kolstela@fmi.fi>
2023-10-13 13:34:35 +0300

    updates

--- python_scripts_container/run_wildfires_wise.py
+++ python_scripts_container/run_wildfires_wise.py
@@ -9,26 +9,33 @@ import csv
 # parser etc. to be added in application integration
 
 # creating the parser
-parser = argparse.ArgumentParser(description='Runscript for data notifier job.')
+#parser = argparse.ArgumentParser(description='Runscript for data notifier job.')
 
 # adding year, month and day arguments
-parser.add_argument('-year_start', required=True, help='Input year', default=1)
-parser.add_argument('-month_start', required=True, help='Input month', default=2)
-parser.add_argument('-day_start', required=True, help='Input day', default=3)
+#parser.add_argument('-year_start', required=True, help='Input year', default=1)
+#parser.add_argument('-month_start', required=True, help='Input month', default=2)
+#parser.add_argument('-day_start', required=True, help='Input day', default=3)
 
-parser.add_argument('-year_end', required=True, help='Input year', default=4)
-parser.add_argument('-month_end', required=True, help='Input month', default=5)
-parser.add_argument('-day_end', required=True, help='Input day', default=6)
+#parser.add_argument('-year_end', required=True, help='Input year', default=4)
+#parser.add_argument('-month_end', required=True, help='Input month', default=5)
+#parser.add_argument('-day_end', required=True, help='Input day', default=6)
 
 
 # parsing the arguments
-args = parser.parse_args()
-year_start = str(args.year_start)
-month_start = str(args.month_start)
-day_start = str(args.day_start)
-year_end = str(args.year_end)
-month_end = str(args.month_end)
-day_end = str(args.day_end)
+#args = parser.parse_args()
+#year_start = str(args.year_start)
+#month_start = str(args.month_start)
+#day_start = str(args.day_start)
+#year_end = str(args.year_end)
+#month_end = str(args.month_end)
+#day_end = str(args.day_end)
+
+year_start = "2020"
+year_end = "2020"
+month_start = "06"
+month_end = "06"
+day_start = "24"
+day_end = "30"
 
 all_dates = [year_start,year_end,month_start,month_end,day_start,day_end]
 
